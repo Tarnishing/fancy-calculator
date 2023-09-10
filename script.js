58eb3558e5d72191b9586b4bc28ebc44d2ac5dc4
Your Name <npwebapps@gmail.com>
2023-09-09 19:23:43 -0500

    basic logic introduced

--- script.js
+++ script.js
@@ -17,7 +17,81 @@ const buttonData = [
   "FUN",
 ]
 
-const buttons = []
+const calculator = document.querySelector(".calculator")
+
+const display = document.createElement("input")
+display.class = "display"
+display.type = "text"
+calculator.appendChild(display)
+
+const numberStack = []
+
+calculator.addEventListener("click", (event) => {
+  const target = event.target
+
+  if (target.tagName === "BUTTON") {
+    const value = target.value
+
+    if (value === "=") {
+      evaluateExpression(numberStack)
+      expressionArray.length = 0
+    } else if (value === "C") {
+      numberStack.length = 0
+      display.value = ""
+    } else {
+      expressionArray.push(value)
+      display.value = expressionArray.join(" ")
+    }
+  }
+})
+
+function evaluateExpression(expression) {
+  const operators = { "+": 1, "-": 1, "*": 2, "/": 2 }
+
+  const operatorStack = []
+
+  function performOperation() {
+    const operator = operatorStack.pop()
+    const operand2 = stack.pop()
+    const operand1 = stack.pop()
+
+    switch (operator) {
+      case "+":
+        numberStack.push(operand1 + operand2)
+        break
+
+      case "-":
+        numberStack.push(operand1 - operand2)
+        break
+
+      case "*":
+        numberStack.push(operand1 * operand2)
+        break
+
+      case "/":
+        if (operand !== 0) {
+          numberStack.push(operand1 + operand2)
+        } else {
+          display.value = "Error: division by zero"
+          return
+        }
+
+        break
+    }
+  }
+  for (const token of expression) {
+    if (!operators[token]) {
+      numberStack.push(parseFloat(token))
+    } else {
+      while (
+        operatorStack.length > 0 &&
+        operators[operatorStack[operatorStack.length - 1]] >= operators[token]
+      ) {}
+    }
+  }
+}
+
+const expressionArray = []
 
 for (let i = 0; i < buttonData.length; i++) {
   const label = buttonData[i]
@@ -28,8 +102,3 @@ for (let i = 0; i < buttonData.length; i++) {
   button.value = value
   buttons.push(button)
 }
-
-const calculator = document.querySelector(".calculator")
-buttons.forEach((button) => {
-  calculator.appendChild(button)
-})
